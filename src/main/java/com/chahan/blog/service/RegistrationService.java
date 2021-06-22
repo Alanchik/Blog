418e4537a40e1f476dfac1483a37e2e656ea4f16
danii <daniil.kastylenka@gmail.com>
2021-06-22 23:31:20 +0300

    constants

--- src/main/java/com/chahan/blog/service/RegistrationService.java
+++ src/main/java/com/chahan/blog/service/RegistrationService.java
@@ -6,6 +6,8 @@ import com.chahan.blog.model.Blogger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static com.chahan.blog.util.Constants.USER_ALREADY_EXISTS;
+
 @Service
 @RequiredArgsConstructor
 public class RegistrationService {
@@ -15,7 +17,7 @@ public class RegistrationService {
     public void signUp(RegistrationDto request) {
         Blogger blogger = bloggerService.getBlogger(request.getUsername());
         if (blogger != null) {
-            throw new BadRequestApiException("User already exists");
+            throw new BadRequestApiException(USER_ALREADY_EXISTS);
         }
         bloggerService.saveNewBlogger(request.getUsername(), request.getPassword());
     }
