5ea8ec9f0beda9972265c520e31f58ad8bacac53
Alan <chagan.alan@gmail.com>
2021-07-17 19:12:53 +0300

    BLG-8 -optimization of structure

--- src/main/java/com/chahan/blog/mapper/BloggerMapper.java
+++ src/main/java/com/chahan/blog/mapper/BloggerMapper.java
@@ -3,9 +3,11 @@ package com.chahan.blog.mapper;
 import com.chahan.blog.dto.AuthorDto;
 import com.chahan.blog.model.Blogger;
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = IGNORE)
 public interface BloggerMapper {
 
     AuthorDto map(Blogger blogger);
